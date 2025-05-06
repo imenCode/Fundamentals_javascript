0941984617c9c6b2cb0b945e7d1ab667d4f9b979
imen-c <imen.cheref@outlook.fr>
2025-05-06 20:26:16 +0200

    challenge3

---
+++ challenge-3.js
@@ -0,0 +1,14 @@
+
+
+// challenge 3 : comparaison moyenne de sscore
+
+let scoreDolphins = ((96 + 108 + 89) / 3);
+let scoreKoalas = ((88 + 91 + 110) / 3);
+
+if (scoreDolphins > scoreKoalas) {
+    console.log("Dolphins win the trophy");
+} else if (scoreDolphins < scoreKoalas) {
+    console.log("Koalas win the trophy");
+} else {
+    console.log("Both win the trophy");
+}
